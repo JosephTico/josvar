eff4e35623ba707f491ccac2c2e53352e27c1880
Joseph Vargas <joseph@cpokemon.com>
2018-02-22 22:22:39 -0600

    added some projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,23 @@ import Helmet from 'react-helmet'
 // import Lightbox from 'react-images'
 import Gallery from '../components/Gallery'
 
-import thumb01 from '../assets/images/thumbs/01.jpg'
-import thumb02 from '../assets/images/thumbs/02.jpg'
+import thumb01 from '../assets/images/thumbs/cp.png'
+import thumb02 from '../assets/images/thumbs/xeon.png'
 import thumb03 from '../assets/images/thumbs/03.jpg'
 import thumb04 from '../assets/images/thumbs/04.jpg'
 import thumb05 from '../assets/images/thumbs/05.jpg'
 import thumb06 from '../assets/images/thumbs/06.jpg'
 
-import full01 from '../assets/images/fulls/01.jpg'
-import full02 from '../assets/images/fulls/02.jpg'
+import full01 from '../assets/images/fulls/cp.png'
+import full02 from '../assets/images/fulls/xeon.png'
 import full03 from '../assets/images/fulls/03.jpg'
 import full04 from '../assets/images/fulls/04.jpg'
 import full05 from '../assets/images/fulls/05.jpg'
 import full06 from '../assets/images/fulls/06.jpg'
 
 const DEFAULT_IMAGES = [
-    { id: '1', src: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '2', src: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '1', src: full01, thumbnail: thumb01, caption: 'Centro Pokémon', description: 'Biggest Spanish-speaking Pokémon community in the world.'},
+    { id: '2', src: full02, thumbnail: thumb02, caption: 'XeonWars', description: 'Shoot \'em up game developedin SFML / C++.'},
     { id: '3', src: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
     { id: '4', src: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
     { id: '5', src: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
